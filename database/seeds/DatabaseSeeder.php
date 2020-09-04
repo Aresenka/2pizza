84fdbd8c07788800c6d79019402efa5d03ea22cc
scilef <kchernenko@affmy.com>
2020-09-04 23:27:30 +0300

    + seeders calls

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,9 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UserSeeder::class);
+        $this->call(CategoriesSeeder::class);
+        $this->call(CurrencySeeder::class);
+        $this->call(MealsSeeder::class);
+        $this->call(MealPricesSeeder::class);
     }
 }
