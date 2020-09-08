cf6d1b783831ff57a55998e819a66ca29d257b26
scilef <kchernenko@affmy.com>
2020-09-08 13:23:23 +0300

    + Login feature + Profile popover

--- resources/js/components/header/nav/main.js
+++ resources/js/components/header/nav/main.js
@@ -1,31 +1,103 @@
-import React from 'react'
-import UserNav from './user/main'
-import AdminNav from './admin/main'
-import Cart from './cart/main'
+import React, {Component} from 'react'
 import {Switch, Route} from "react-router-dom"
+import UserNav from './user/main'
+import Cart from './cart/main'
+import Modal from './user/login'
+import Profile from './user/profile'
+import AdminNav from './admin/main'
 
-export default props => {
-    return (
-        <div>
-            <nav className="nav-menu">
-                <Switch>
-                    <Route exact path='/'>
-                        <UserNav empty={props.cart.filter(item => item).length === 0}/>
-                        <Cart
-                            items={props.cart}
-                            currency={props.currency}
-                            cartIsOpen={props.cartIsOpen}
-                            toggleCart={props.toggleCart}
-                            changeCount={props.changeCount}
-                            removeItem={props.removeItem}
-                            checkoutOrder={props.checkoutOrder}
-                        />
-                    </Route>
-                    <Route path='/admin'>
-                        <AdminNav/>
-                    </Route>
-                </Switch>
-            </nav>
-        </div>
-    )
+export default class Nav extends Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            showModal: false,
+            showProfile: false,
+        }
+
+        this.handleClick = this.handleClick.bind(this)
+        this.handleLogout = this.handleLogout.bind(this)
+        this.toggleProfile = this.toggleProfile.bind(this)
+    }
+
+    componentDidMount() {
+        this.setState({
+            showModal: this.props.loginIsOpen
+        })
+    }
+
+    componentDidUpdate(prevProps, prevState) {
+        if(prevState.showModal !== this.props.loginIsOpen){
+            this.setState({
+                showModal: this.props.loginIsOpen
+            })
+        }
+    }
+
+    handleClick() {
+        let authed = JSON.parse(localStorage['appState']).authed
+
+        if (!authed) {
+            this.setState({
+                showModal: true
+            })
+        }
+    }
+
+    handleLogout(){
+        this.props.disauthUser()
+        this.setState({
+            showProfile: false
+        })
+    }
+
+    toggleProfile(){
+        let authed = JSON.parse(localStorage['appState']).authed
+
+        if(authed){
+            this.setState({
+                showProfile: !this.state.showProfile
+            })
+        }
+    }
+
+    render() {
+        return (
+            <div>
+                <nav className="nav-menu">
+                    <Switch>
+                        <Route exact path='/'>
+                            <UserNav
+                                empty={this.props.cart.filter(item => item).length === 0}
+                                handleClick={this.handleClick}
+                            />
+                            <Cart
+                                items={this.props.cart}
+                                currency={this.props.currency}
+                                cartIsOpen={this.props.cartIsOpen}
+                                toggleCart={this.props.toggleCart}
+                                changeCount={this.props.changeCount}
+                                removeItem={this.props.removeItem}
+                                checkoutOrder={this.props.checkoutOrder}
+                            />
+                            <Profile
+                                isOpen={this.state.showProfile}
+                                toggle={this.toggleProfile}
+                                logout={this.handleLogout}
+                            />
+                            <Modal
+                                error={this.props.authError}
+                                showModal={this.state.showModal}
+                                closeModal={this.props.closeLogin}
+                                authUser={this.props.authUser}
+                            />
+                        </Route>
+                        <Route path='/admin'>
+                            <AdminNav/>
+                        </Route>
+                    </Switch>
+                </nav>
+            </div>
+        )
+    }
 }
