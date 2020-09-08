6b87a65cb3efae2336a3ab879e742c89514a907b
scilef <kchernenko@affmy.com>
2020-09-08 13:16:38 +0300

    = Fixed routing

--- resources/js/components/body/admin/main.js
+++ resources/js/components/body/admin/main.js
@@ -1,24 +1,29 @@
 import React from 'react'
-import {Switch, Route, Redirect, useRouteMatch} from "react-router-dom"
+import {Switch, Route, Redirect} from "react-router-dom"
 import Orders from './orders/main'
 
 export default props => {
-    let match = useRouteMatch()
-
+    let authed = JSON.parse(localStorage['appState']).authed
     return (
-        <main
-            id='main'
-            className='admin'
-        >
-            <Switch>
-                <Route exact path={match.path}>
-                    <Redirect to={`${match.path}/orders`}/>
-                </Route>
-                <Route path={`${match.path}/orders`}>
-                    <Orders />
-                </Route>
-                <Redirect to={`${match.path}/orders`}/>
-            </Switch>
-        </main>
+        <Route
+            path={props.path}
+            render={() => authed === true ?
+                <main
+                    id='main'
+                    className='admin'
+                >
+                    <Switch>
+                        <Route exact path={props.path}>
+                            <Redirect to={`${props.path}/orders`}/>
+                        </Route>
+                        <Route path={`${props.path}/orders`}>
+                            <Orders/>
+                        </Route>
+                        <Redirect to={`${props.path}/orders`}/>
+                    </Switch>
+                </main> :
+                <Redirect to='/'/>
+            }
+        />
     )
 }
